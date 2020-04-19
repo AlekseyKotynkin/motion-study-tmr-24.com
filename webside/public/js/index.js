6b08a94db4871c2ac480c420def4e38b2a1aec78
AlekseyKotynkin <cay211076@gmail.com>
2020-04-19 18:09:54 +0300

    2020 04 19 18 09

---
+++ webside/public/js/index.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyBRd2yCJouL2GTNXQXuIiruefG40mixJeY",
+   authDomain: "motion-study-tmr-24.firebaseapp.com",
+   databaseURL: "https://motion-study-tmr-24.firebaseio.com",
+   projectId: "motion-study-tmr-24",
+   storageBucket: "motion-study-tmr-24.appspot.com",
+   messagingSenderId: "475611275877",
+   appId: "1:475611275877:web:c864a0fa3746694f2edc8c",
+   measurementId: "G-T3J9JSRXL6"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
