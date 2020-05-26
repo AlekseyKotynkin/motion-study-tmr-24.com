ed58eb86c8e818633faad925a3aa26995b7048b3
AlekseyKotynkin <cay211076@gmail.com>
2020-05-26 18:26:54 +0300

    2020 05 26 18 26

--- webside/public/commonAdminOrganization.js
+++ webside/public/commonAdminOrganization.js
@@ -103,76 +103,65 @@ docRef.get().then(function(doc) {
     * @return {string}
      *  Получение данных для таблицы List Of Organizations In Which You Are Involved из firestore.
      */
+  function createATableOfClientSubdivision()
+  {
+    docRef.collection("Subdivision")
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        items.push({...doc.data(),...{idSubdivision: doc.id}});
+      });
 
-      function createATableOfClientSubdivision()
-      {
-      db.collection("Organization").where("OwnerEmail", "==", EmailLocalStorage)
-      .get()
-      .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-          items.push({...doc.data(),...{idOrganization: doc.id}});
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        })
+          .finally(() => {items;
+        items.forEach(item => {
+          var tr = document.createElement("tr");
+
+          var subdivisionColumn = document.createElement('td');
+          subdivisionColumn.innerHTML = item.Subdivision;
+
+          var nameOfDepartmentHeadColumn = document.createElement('td');
+          nameOfDepartmentHeadColumn.innerHTML = item.NameOfDepartmentHead;
+
+          var headOfUnitColumn = document.createElement('td');
+          headOfUnitColumn.innerHTML = item.HeadOfUnit;
+
+
+          var theNumberOfEmployeesColumn = document.createElement('td');
+          theNumberOfEmployeesColumn.innerHTML = item.StatusUser;
+
+          var toComeInUserName = document.createElement('button');
+          toComeInUserName.innerHTML = "To come in";
+          toComeInUserName.className = 'badge badge-gradient-success';
+          toComeInUserName.id = item.idSubdivision;
+          toComeInUserName.setAttribute('onclick', 'toComeInButtonSubdivision(this)');
+
+          var toComeInUserColumn = document.createElement('td');
+          toComeInUserColumn.appendChild(toComeInUserName);
+
+          var quitName = document.createElement('button');
+          quitName.innerHTML = "Quit";
+          quitName.className = 'badge badge-gradient-danger';
+          quitName.id = item.idSubdivision;
+          quitName.setAttribute('onclick', 'quitButtonSubdivision(this)');
+
+          var quitColumn = document.createElement('td');
+          quitColumn.appendChild(quitName);
+
+          tr.appendChild(subdivisionColumn);
+          tr.appendChild(nameOfDepartmentHeadColumn);
+          tr.appendChild(headOfUnitColumn);
+          tr.appendChild(theNumberOfEmployeesColumn);
+          tr.appendChild(toComeInUserColumn);
+          tr.appendChild(quitColumn);
+
+          container.appendChild(tr);
         });
-
-          })
-          .catch(function(error) {
-              console.log("Error getting documents: ", error);
-          })
-            .finally(() => {items;
-          items.forEach(item => {
-            var tr = document.createElement("tr");
-
-            var organizationName = document.createElement('a');
-            organizationName.innerHTML = item.Organization;
-
-            var organizationColumn = document.createElement('td');
-            organizationColumn.appendChild(organizationName);
-
-            var subdivisionColumn = document.createElement('td');
-            subdivisionColumn.innerHTML = item.Subdivision;
-
-            var positionColumn = document.createElement('td');
-            positionColumn.innerHTML = item.Position;
-
-            var positionOfYourManagerColumn = document.createElement('td');
-            positionOfYourManagerColumn.innerHTML = item.PositionOfYourManager;
-
-            var nameOfYourManagerColumn = document.createElement('td');
-            nameOfYourManagerColumn.innerHTML = item.NameOfYourManager;
-
-            var statusUserColumn = document.createElement('td');
-            statusUserColumn.innerHTML = item.StatusUser;
-
-            var toComeInUserName = document.createElement('button');
-            toComeInUserName.innerHTML = "To come in";
-            toComeInUserName.className = 'badge badge-gradient-success';
-            toComeInUserName.id = item.idOrganization;
-            toComeInUserName.setAttribute('onclick', 'toComeInButton(this)');
-
-            var toComeInUserColumn = document.createElement('td');
-            toComeInUserColumn.appendChild(toComeInUserName);
-
-            var quitName = document.createElement('button');
-            quitName.innerHTML = "Quit";
-            quitName.className = 'badge badge-gradient-danger';
-            quitName.id = item.idOrganization;
-            quitName.setAttribute('onclick', 'quitButton(this)');
-
-            var quitColumn = document.createElement('td');
-            quitColumn.appendChild(quitName);
-
-            tr.appendChild(organizationColumn);
-            tr.appendChild(subdivisionColumn);
-            tr.appendChild(positionColumn);
-            tr.appendChild(positionOfYourManagerColumn);
-            tr.appendChild(nameOfYourManagerColumn);
-            tr.appendChild(statusUserColumn);
-            tr.appendChild(toComeInUserColumn);
-            tr.appendChild(quitColumn);
-
-            container.appendChild(tr);
-          });
-        });
-    };
+      });
+  };
 
 
 
@@ -183,15 +172,15 @@ docRef.get().then(function(doc) {
 
   function toComeInButtonSubdivision(obj) {
     //обработка редактирования строки...
-      let objId = obj.id;
-      alert(obj.id);
-        let itemsArray = [{
-          OrganizationId: objId,
-          OwnerEmail: EmailLocalStorage,
-          ProviderId: "TMR-24.com"
-        }]
-      localStorage.setItem('TMR::rememberedAdmin', JSON.stringify(itemsArray));
-      window.location.replace("indexAdminOrganization.html");
+      // let objId = obj.id;
+      // alert(obj.id);
+      //   let itemsArray = [{
+      //     OrganizationId: objId,
+      //     OwnerEmail: EmailLocalStorage,
+      //     ProviderId: "TMR-24.com"
+      //   }]
+      // localStorage.setItem('TMR::rememberedAdmin', JSON.stringify(itemsArray));
+      // window.location.replace("indexAdminOrganization.html");
     }
 
     /**
@@ -202,7 +191,7 @@ docRef.get().then(function(doc) {
     function quitButtonSubdivision(obj) {
     let objId = obj.id;
     alert('Document successfully deleted! '+ (objId));
-      db.collection("Organization").doc(objId).delete().then(function() {
+    docRef.collection("Organization").doc(objId).delete().then(function() {
           console.log("Document successfully deleted!");
       }).catch(function(error) {
           console.error("Error removing document: ", error);
