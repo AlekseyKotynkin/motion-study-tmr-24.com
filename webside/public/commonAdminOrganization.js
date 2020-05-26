6f0895a2adb6cae909982542e2d2cdf52c2e8f3d
AlekseyKotynkin <cay211076@gmail.com>
2020-05-26 17:12:47 +0300

    2020 05 26 17 12

--- webside/public/commonAdminOrganization.js
+++ webside/public/commonAdminOrganization.js
@@ -16,6 +16,7 @@
  * Общие методы для главной страницы приложения и автономного виджета.
  */
 let documentData=[];
+let items=[];
 // let documentData;
 // let d;
 
@@ -28,10 +29,18 @@ const UserNamelocalStorage = (LocalStorageValueObject[0]).displayName;
 const EmailLocalStorage = (LocalStorageValueObject[0]).email;
 const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
 
+/**
+* @return {string}
+*  Читаем параметры из localStorage 'TMR::rememberedAdmin'.
+*/
 const LocalStorageValueObjectOrganization = JSON.parse(localStorage.getItem('TMR::rememberedAdmin'));
 const localStorageOrganizationId = (LocalStorageValueObjectOrganization[0]).OrganizationId;
 const LocalStorageEmailOrganization = (LocalStorageValueObjectOrganization[0]).OwnerEmail;
 
+/**
+* @return {string}
+*  Заполняем шапку табличной части Подразделения.
+*/
 
 var docRef = db.collection("Organization").doc(localStorageOrganizationId);
 docRef.get().then(function(doc) {
@@ -45,13 +54,12 @@ docRef.get().then(function(doc) {
 })
   .finally(() => {documentData;
      documentData.forEach(item => {
-        my_div = document.getElementById("org_div1");
+        my_div = document.getElementById("headerTableSubdivision");
         const ul = my_div.querySelector("h4");
         let li = item.Organization;
         ul.insertAdjacentHTML("beforeend", li);
     });
 });
-
 
 /**
 * @return {string}
@@ -59,6 +67,7 @@ docRef.get().then(function(doc) {
  */
  function SignoutAdmin() {
    localStorage.clear('firebaseui::rememberedAccounts');
+   localStorage.clear('TMR::rememberedAdmin');
    window.location.replace("index.html")
  }
 
@@ -67,21 +76,16 @@ docRef.get().then(function(doc) {
   *  Обработка модального окна Регистрация Организации.
   */
 
-  function gridSystemModalNewOrganization()
+  function gridSystemModalNewSubdivision()
   {
-    var Organization = document.getElementById("exampleInputNameOrganization").value;
-    var Position = document.getElementById("exampleInputPosition").value;
-    // Добавляем в коллекциию новую организацию и данные основателя.
-    db.collection("Organization").add({
-    StatusUser: "StatusUser_Owner",
-    Organization: Organization,
-    Subdivision: "",
-    Position: Position,
-    OwnerEmail: EmailLocalStorage,
-    OwnerID: "",
-    PositionOfYourManager: "",
-    NameOfYourManager: "",
-    // DocumentCreationTimes: FieldValue.serverTimestamp(),
+    var subdivisionTitle = document.getElementById("exampleInputModalSubdivisionTitle").value;
+    var nameOfDepartmentHead = document.getElementById("exampleInputModalSubdivisionNameOfDepartamentHead").value;
+    var headOfUnit = document.getElementById("exampleInputModalSubdivisionHeadOfUnit").value;
+    // Добавляем в коллекциию организации Подразделения и данные руководителя.
+    docRef.collection("Subdivision").add({
+    Subdivision: subdivisionTitle,
+    NameOfDepartmentHead: nameOfDepartmentHead,
+    HeadOfUnit: headOfUnit,
 
     })
     .then(function(docRef) {
@@ -100,73 +104,76 @@ docRef.get().then(function(doc) {
      *  Получение данных для таблицы List Of Organizations In Which You Are Involved из firestore.
      */
 
-    // db.collection("Organization").where("OwnerEmail", "==", EmailLocalStorage)
-    // .get()
-    // .then(function(querySnapshot) {
-    //   querySnapshot.forEach(function(doc) {
-    //     items.push({...doc.data(),...{idOrganization: doc.id}});
-    //   });
-    //
-    //     })
-    //     .catch(function(error) {
-    //         console.log("Error getting documents: ", error);
-    //     })
-    //       .finally(() => {items;
-    //     items.forEach(item => {
-    //       var tr = document.createElement("tr");
-    //
-    //       var organizationName = document.createElement('a');
-    //       organizationName.innerHTML = item.Organization;
-    //
-    //       var organizationColumn = document.createElement('td');
-    //       organizationColumn.appendChild(organizationName);
-    //
-    //       var subdivisionColumn = document.createElement('td');
-    //       subdivisionColumn.innerHTML = item.Subdivision;
-    //
-    //       var positionColumn = document.createElement('td');
-    //       positionColumn.innerHTML = item.Position;
-    //
-    //       var positionOfYourManagerColumn = document.createElement('td');
-    //       positionOfYourManagerColumn.innerHTML = item.PositionOfYourManager;
-    //
-    //       var nameOfYourManagerColumn = document.createElement('td');
-    //       nameOfYourManagerColumn.innerHTML = item.NameOfYourManager;
-    //
-    //       var statusUserColumn = document.createElement('td');
-    //       statusUserColumn.innerHTML = item.StatusUser;
-    //
-    //       var toComeInUserName = document.createElement('button');
-    //       toComeInUserName.innerHTML = "To come in";
-    //       toComeInUserName.className = 'badge badge-gradient-success';
-    //       toComeInUserName.id = item.idOrganization;
-    //       toComeInUserName.setAttribute('onclick', 'toComeInButton(this)');
-    //
-    //       var toComeInUserColumn = document.createElement('td');
-    //       toComeInUserColumn.appendChild(toComeInUserName);
-    //
-    //       var quitName = document.createElement('button');
-    //       quitName.innerHTML = "Quit";
-    //       quitName.className = 'badge badge-gradient-danger';
-    //       quitName.id = item.idOrganization;
-    //       quitName.setAttribute('onclick', 'quitButton(this)');
-    //
-    //       var quitColumn = document.createElement('td');
-    //       quitColumn.appendChild(quitName);
-    //
-    //       tr.appendChild(organizationColumn);
-    //       tr.appendChild(subdivisionColumn);
-    //       tr.appendChild(positionColumn);
-    //       tr.appendChild(positionOfYourManagerColumn);
-    //       tr.appendChild(nameOfYourManagerColumn);
-    //       tr.appendChild(statusUserColumn);
-    //       tr.appendChild(toComeInUserColumn);
-    //       tr.appendChild(quitColumn);
-    //
-    //       container.appendChild(tr);
-    //     });
-    //   });
-  // };
+      function createATableOfClientSubdivision()
+      {
+      db.collection("Organization").where("OwnerEmail", "==", EmailLocalStorage)
+      .get()
+      .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          items.push({...doc.data(),...{idOrganization: doc.id}});
+        });
+
+          })
+          .catch(function(error) {
+              console.log("Error getting documents: ", error);
+          })
+            .finally(() => {items;
+          items.forEach(item => {
+            var tr = document.createElement("tr");
+
+            var organizationName = document.createElement('a');
+            organizationName.innerHTML = item.Organization;
+
+            var organizationColumn = document.createElement('td');
+            organizationColumn.appendChild(organizationName);
+
+            var subdivisionColumn = document.createElement('td');
+            subdivisionColumn.innerHTML = item.Subdivision;
+
+            var positionColumn = document.createElement('td');
+            positionColumn.innerHTML = item.Position;
+
+            var positionOfYourManagerColumn = document.createElement('td');
+            positionOfYourManagerColumn.innerHTML = item.PositionOfYourManager;
+
+            var nameOfYourManagerColumn = document.createElement('td');
+            nameOfYourManagerColumn.innerHTML = item.NameOfYourManager;
+
+            var statusUserColumn = document.createElement('td');
+            statusUserColumn.innerHTML = item.StatusUser;
+
+            var toComeInUserName = document.createElement('button');
+            toComeInUserName.innerHTML = "To come in";
+            toComeInUserName.className = 'badge badge-gradient-success';
+            toComeInUserName.id = item.idOrganization;
+            toComeInUserName.setAttribute('onclick', 'toComeInButton(this)');
+
+            var toComeInUserColumn = document.createElement('td');
+            toComeInUserColumn.appendChild(toComeInUserName);
+
+            var quitName = document.createElement('button');
+            quitName.innerHTML = "Quit";
+            quitName.className = 'badge badge-gradient-danger';
+            quitName.id = item.idOrganization;
+            quitName.setAttribute('onclick', 'quitButton(this)');
+
+            var quitColumn = document.createElement('td');
+            quitColumn.appendChild(quitName);
+
+            tr.appendChild(organizationColumn);
+            tr.appendChild(subdivisionColumn);
+            tr.appendChild(positionColumn);
+            tr.appendChild(positionOfYourManagerColumn);
+            tr.appendChild(nameOfYourManagerColumn);
+            tr.appendChild(statusUserColumn);
+            tr.appendChild(toComeInUserColumn);
+            tr.appendChild(quitColumn);
+
+            container.appendChild(tr);
+          });
+        });
+    };
+
 
 
   /**
@@ -174,31 +181,31 @@ docRef.get().then(function(doc) {
    *  Обработчик кнопки toComeInUserColumn из таблицы List Of Organizations In Which You Are Involved.
    */
 
-  // function toComeInButtonSubdivision(obj) {
-  //   //обработка редактирования строки...
-  //     let objId = obj.id;
-  //     alert(obj.id);
-  //       let itemsArray = [{
-  //         OrganizationId: objId,
-  //         OwnerEmail: EmailLocalStorage,
-  //         ProviderId: "TMR-24.com"
-  //       }]
-  //     localStorage.setItem('TMR::rememberedAdmin', JSON.stringify(itemsArray));
-  //     window.location.replace("indexAdminOrganization.html");
-  //   }
-  //
-  //   /**
-  //   * @return {string}
-  //    *  Обработчик кнопки quitColumn из таблицы List Of Organizations In Which You Are Involved.
-  //    */
-  //
-  //   function quitButtonSubdivision(obj) {
-  //   let objId = obj.id;
-  //   alert('Document successfully deleted! '+ (objId));
-  //     db.collection("Organization").doc(objId).delete().then(function() {
-  //         console.log("Document successfully deleted!");
-  //     }).catch(function(error) {
-  //         console.error("Error removing document: ", error);
-  //     });
-  //     window.location.reload();
-  //   }
+  function toComeInButtonSubdivision(obj) {
+    //обработка редактирования строки...
+      let objId = obj.id;
+      alert(obj.id);
+        let itemsArray = [{
+          OrganizationId: objId,
+          OwnerEmail: EmailLocalStorage,
+          ProviderId: "TMR-24.com"
+        }]
+      localStorage.setItem('TMR::rememberedAdmin', JSON.stringify(itemsArray));
+      window.location.replace("indexAdminOrganization.html");
+    }
+
+    /**
+    * @return {string}
+     *  Обработчик кнопки quitColumn из таблицы List Of Organizations In Which You Are Involved.
+     */
+
+    function quitButtonSubdivision(obj) {
+    let objId = obj.id;
+    alert('Document successfully deleted! '+ (objId));
+      db.collection("Organization").doc(objId).delete().then(function() {
+          console.log("Document successfully deleted!");
+      }).catch(function(error) {
+          console.error("Error removing document: ", error);
+      });
+      window.location.reload();
+    }
