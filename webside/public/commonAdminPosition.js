b45b9f47baef5add0a1ca515549f5f7ff7e7dca9
AlekseyKotynkin <cay211076@gmail.com>
2020-05-30 19:13:45 +0300

    2020 05 30 19 13

--- webside/public/commonAdminPosition.js
+++ webside/public/commonAdminPosition.js
@@ -19,7 +19,7 @@ let documentData=[];
 let documentDataSubdivision=[];
 let items=[];
 let itemsPosition=[];
-let localStorageSubdivision = 0;
+
 // let docRef=0 ;
 // let docRefFull=0 ;
 
@@ -45,13 +45,23 @@ const LocalStorageEmailOrganization = (LocalStorageValueObjectOrganization[0]).O
 * @return {string}
 *  Читаем параметры из localStorage 'TMR::rememberedAdminSubdivision'.
 */
+const LocalStorageValueObjectSubdivision = JSON.parse(localStorage.getItem('TMR::rememberedAdminSubdivision'));
+const localStorageSubdivision = (LocalStorageValueObjectOrganization[0]).SubdivisionId;
+
+/**
+* @return {string}
+*  Читаем параметры из localStorage 'TMR::rememberedAdminPosition'.
+*/
+const LocalStorageValueObjectPosition = JSON.parse(localStorage.getItem('TMR::rememberedAdminPosition'));
+const localStoragePosition = (LocalStorageValueObjectOrganization[0]).PositionId;
 
 /**
 * @return {string}
 *  Заполняем шапку табличной части Подразделения.
 */
 
-  var docRef = db.collection("Organization").doc(localStorageOrganizationId);
+  var docRef = db.collection("Organization").doc(localStorageOrganizationId).collection("Subdivision").doc(localStorageSubdivision).collection("Position").doc(localStoragePosition);
+    console.log(docRef);
     docRef.get().then(function(doc) {
     if (doc.exists) {
         documentData.push(doc.data());
@@ -62,12 +72,12 @@ const LocalStorageEmailOrganization = (LocalStorageValueObjectOrganization[0]).O
         console.log("Error getting document:", error);
     })
     .finally(() => {documentData;
-     documentData.forEach(item => {
-        my_div = document.getElementById("headerTableSubdivision");
-        const ul = my_div.querySelector("h4");
-        let li = item.Organization;
-        ul.insertAdjacentHTML("beforeend", li);
-    });
+     // documentData.forEach(item => {
+     //    my_div = document.getElementById("headerTableSubdivision");
+     //    const ul = my_div.querySelector("h4");
+     //    let li = item.Organization;
+     //    ul.insertAdjacentHTML("beforeend", li);
+    // });
   });
 
 
@@ -80,275 +90,3 @@ const LocalStorageEmailOrganization = (LocalStorageValueObjectOrganization[0]).O
 
    window.location.replace("index.html")
  };
-
- /**
- * @return {string}
-  *  Обработка модального окна Регистрация Подразделения.
-  */
-
-  function gridSystemModalNewSubdivision()
-  {
-    var subdivisionTitle = document.getElementById("exampleInputModalSubdivisionTitle").value;
-    var nameOfDepartmentHead = document.getElementById("exampleInputModalSubdivisionNameOfDepartamentHead").value;
-    var headOfUnit = document.getElementById("exampleInputModalSubdivisionHeadOfUnit").value;
-    // Добавляем в коллекциию организации Подразделения и данные руководителя.
-    docRef.collection("Subdivision").add({
-    Subdivision: subdivisionTitle,
-    NameOfDepartmentHead: nameOfDepartmentHead,
-    HeadOfUnit: headOfUnit,
-
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        alert("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-        alert("Error adding document: ", error);
-    });
-  };
-
-
-    /**
-    * @return {string}
-     *  Получение данных для таблицы список Подразделений List Of Subdivision In Which You Are Involved из firestore.
-     */
-  function createATableOfClientSubdivision()
-  {
-    docRef.collection("Subdivision")
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        items.push({...doc.data(),...{idSubdivision: doc.id}});
-      });
-
-        })
-        .catch(function(error) {
-            console.log("Error getting documents: ", error);
-        })
-          .finally(() => {items;
-        items.forEach(item => {
-          var tr = document.createElement("tr");
-
-          var subdivisionColumn = document.createElement('td');
-          subdivisionColumn.innerHTML = item.Subdivision;
-
-          var nameOfDepartmentHeadColumn = document.createElement('td');
-          nameOfDepartmentHeadColumn.innerHTML = item.NameOfDepartmentHead;
-
-          var headOfUnitColumn = document.createElement('td');
-          headOfUnitColumn.innerHTML = item.HeadOfUnit;
-
-
-          var theNumberOfEmployeesColumn = document.createElement('td');
-          theNumberOfEmployeesColumn.innerHTML = item.StatusUser;
-
-          var toComeInUserName = document.createElement('button');
-          toComeInUserName.innerHTML = "To come in";
-          toComeInUserName.className = 'badge badge-gradient-success';
-          toComeInUserName.id = item.idSubdivision;
-          toComeInUserName.setAttribute('onclick', 'toComeInButtonSubdivision(this)');
-
-          var toComeInUserColumn = document.createElement('td');
-          toComeInUserColumn.appendChild(toComeInUserName);
-
-          var quitName = document.createElement('button');
-          quitName.innerHTML = "Quit";
-          quitName.className = 'badge badge-gradient-danger';
-          quitName.id = item.idSubdivision;
-          quitName.setAttribute('onclick', 'quitButtonSubdivision(this)');
-
-          var quitColumn = document.createElement('td');
-          quitColumn.appendChild(quitName);
-
-          tr.appendChild(subdivisionColumn);
-          tr.appendChild(nameOfDepartmentHeadColumn);
-          tr.appendChild(headOfUnitColumn);
-          tr.appendChild(theNumberOfEmployeesColumn);
-          tr.appendChild(toComeInUserColumn);
-          tr.appendChild(quitColumn);
-
-          container.appendChild(tr);
-        });
-      });
-  };
-
-
-
-  /**
-  // * @return {string}
-   *  Обработчик кнопки toComeInUserColumn из таблицы List Of Subdivision In Which You Are Involved.
-   */
-
-  function toComeInButtonSubdivision(obj) {
-  //обработка редактирования строки...
-    localStorage.removeItem('TMR::rememberedAdminSubdivision');
-    // var removeTab = document.getElementById('headerTablePosition').parentElement.removeChild(removeTab);
-    // document.getElementById("headerTablePosition").reset();
-
-    var docRefFull = db.collection("Organization").doc(localStorageOrganizationId).collection("Subdivision").doc(obj.id);
-    docRefFull.get().then(function(doc) {
-      if (doc.exists) {
-        documentDataSubdivision.push({...doc.data(),...{idSubdivision: doc.id}});
-      } else {
-        console.log("No such document!");
-      }
-      }).catch(function(error) {
-        console.log("Error getting document:", error);
-      })
-      .finally(() => {documentDataSubdivision;
-       documentDataSubdivision.forEach(item => {
-          document.body.innerHTML = document.body.innerHTML.replace('- Select subdivision', '- ');
-          my_div = document.getElementById("headerTablePosition");
-          const ul = my_div.querySelector("h4");
-          let li = item.Subdivision;
-
-          let lit = '<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#gridSystemModalNewPosition">+ Add Position</button>';
-          ul.insertAdjacentHTML("beforeend", li);
-          ul.insertAdjacentHTML("afterend", lit);
-
-          localStorageSubdivision = item.idSubdivision;
-          console.log(localStorageSubdivision);
-            let itemsArray = [{
-              SubdivisionId: localStorageSubdivision,
-              OwnerEmail: EmailLocalStorage,
-              ProviderId: "TMR-24.com"
-            }]
-          localStorage.setItem('TMR::rememberedAdminSubdivision', JSON.stringify(itemsArray));
-
-      });
-
-     docRefFull.collection("Position")
-     .get()
-     .then(function(querySnapshot) {
-       querySnapshot.forEach(function(doc) {
-         itemsPosition.push({...doc.data(),...{idPosition: doc.id}});
-       });
-
-         })
-         .catch(function(error) {
-             console.log("Error getting documents: ", error);
-         })
-           .finally(() => {itemsPosition;
-           itemsPosition.forEach(item => {
-           var tr = document.createElement("tr");
-
-           var positionColumn = document.createElement('td');
-           positionColumn.innerHTML = item.Position;
-
-           var positionCommentColumn = document.createElement('td');
-           positionCommentColumn.innerHTML = item.PositionComment;
-
-           var theNumberOfEmployeesColumn = document.createElement('td');
-           theNumberOfEmployeesColumn.innerHTML = item.StatusUser;
-
-           var toComeInUserName = document.createElement('button');
-           toComeInUserName.innerHTML = "To come in";
-           toComeInUserName.className = 'badge badge-gradient-success';
-           toComeInUserName.id = item.idPosition;
-           toComeInUserName.setAttribute('onclick', 'toComeInButtonPosition(this)');
-
-           var toComeInUserColumn = document.createElement('td');
-           toComeInUserColumn.appendChild(toComeInUserName);
-
-           var quitName = document.createElement('button');
-           quitName.innerHTML = "Quit";
-           quitName.className = 'badge badge-gradient-danger';
-           quitName.id = item.idPosition;
-           quitName.setAttribute('onclick', 'quitButtonPosition(this)');
-
-           var quitColumn = document.createElement('td');
-           quitColumn.appendChild(quitName);
-
-           tr.appendChild(positionColumn);
-           tr.appendChild(positionCommentColumn);
-           tr.appendChild(theNumberOfEmployeesColumn);
-           tr.appendChild(toComeInUserColumn);
-           tr.appendChild(quitColumn);
-
-           var container = document.getElementById("tablePosition").getElementsByTagName("tbody")[0];
-
-           container.appendChild(tr);
-         });
-       });
-    });
-  };
-
-
-    /**
-    * @return {string}
-     *  Обработчик кнопки quitColumn из таблицы List Of Subdivision In Which You Are Involved.
-     */
-
-    function quitButtonSubdivision(obj) {
-    localStorage.removeItem('TMR::rememberedAdminSubdivision');
-    let objId = obj.id;
-    alert('Document successfully deleted! '+ (objId));
-    docRef.collection("Subdivision").doc(objId).delete().then(function() {
-          console.log("Document successfully deleted!");
-      }).catch(function(error) {
-          console.error("Error removing document: ", error);
-      });
-      window.location.reload();
-    };
-
-
-    /**
-    * @return {string}
-     *  Обработка модального окна Регистрация Должности.
-     */
-
-     function gridSystemModalNewPosition()
-     {
-       var position_Title = document.getElementById("exampleInputModalPositionTitle").value;
-       var position_Comment = document.getElementById("exampleInputModalPositionСomment").value;
-       // Добавляем в документ Подразделения должность.
-       docRefFull.collection("Position").add({
-       Position: position_Title,
-       PositionComment: position_Comment,
-
-       })
-       .then(function(docRefFull) {
-           console.log("Document written with ID: ", docRefFull.id);
-           alert("Document written with ID: ", docRefFull.id);
-       })
-       .catch(function(error) {
-           console.error("Error adding document: ", error);
-           alert("Error adding document: ", error);
-       });
-     };
-
-   /**
-   // * @return {string}
-    *  Обработчик кнопки toComeInUserColumn из таблицы List Of Organizations In Which You Are Involved.
-    */
-
-   function toComeInButtonPosition(obj) {
-     //обработка редактирования строки...
-       let objId = obj.id;
-       console.log(obj);
-
-         let itemsArray = [{
-           PositionId: objId,
-           OwnerEmail: EmailLocalStorage,
-           ProviderId: "TMR-24.com"
-         }]
-       localStorage.setItem('TMR::rememberedAdminPosition', JSON.stringify(itemsArray));
-       window.location.replace("indexAdminPosition.html");
-     };
-     /**
-     * @return {string}
-      *  Обработчик кнопки quitColumn из таблицы List Of Organizations In Which You Are Involved.
-      */
-
-     function quitButtonPosition(obj) {
-       let objId = obj.id;
-       console.log(localStorageSubdivision);
-       alert('Document successfully deleted! '+ (objId));
-       docRef.collection("Subdivision").doc(localStorageSubdivision).collection("Position").doc(objId).delete().then(function() {
-             console.log("Document successfully deleted!");
-         }).catch(function(error) {
-             console.error("Error removing document: ", error);
-         });
-         window.location.reload();
-       };
