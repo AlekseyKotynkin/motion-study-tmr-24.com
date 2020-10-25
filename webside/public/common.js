7cfe87ef6da0491f9c01e879983d413d96edb4b4
AlekseyKotynkin <cay211076@gmail.com>
2020-10-25 06:51:01 +0300

    2020 10 25 06 50

--- webside/public/common.js
+++ webside/public/common.js
@@ -79,7 +79,8 @@ function parseQueryString(queryString) {
      var email = document.getElementById("exampleInputEmail1").value;
      var password = document.getElementById("exampleInputPassword1").value;
        // sign up the Username
-     db.auth().createUserWithEmailAndPassword(email, password).catch(function(error)
+       // регистрируем имя пользователя
+     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error)
      {
        // Handle Errors here.
        var errorCode = error.code;
