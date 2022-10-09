6edde115fd39c40d1a76dff8701e7da1db820383
AlekseyKotynkin <cay211076@gmail.com>
2022-10-09 10:53:18 +0300

    2022_10_09 10_53

---
+++ webside/public/js/translation.js
@@ -0,0 +1,24 @@
+var arrLang = {
+  'en': {
+    'login': 'Login',
+    'register': 'Register',
+    'knowledge_base': 'Knowledge base',
+    'contact': 'Contact US',
+  },
+  'ru': {
+    'login': 'Вход',
+    'register': 'Регистрация',
+    'knowledge_base': 'База знаний',
+    'contact': 'Контакты',
+  }
+};
+
+  $(function() {
+    $('.translate').click(function() {
+      var lang = $(this).attr('id');
+
+      $('.lang').each(function(index, item) {
+        $(this).text(arrLang[lang][$(this).attr('key')]);
+      });
+    });
+  });
