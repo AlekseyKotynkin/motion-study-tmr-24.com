1eee4cedddcc6228db021fb5506ddc5c4d264355
AlekseyKotynkin <cay211076@gmail.com>
2022-10-09 15:15:15 +0300

    2022_10_09 15_15

--- webside/public/js/translation.js
+++ webside/public/js/translation.js
@@ -1,24 +1,76 @@
+function translation_RU (){
+  var lang = 'ru';
+
+  $('.lang').each(function(index, item) {
+    $(this).text(arrLang[lang][$(this).attr('key')]);
+  });
+  if (localStorage.length > 0)
+  {
+    for(let key in localStorage) {
+     if (!localStorage.hasOwnProperty(key)) {
+     continue; // пропустит такие ключи, как "setItem", "getItem" и так далее
+       }
+         if (key === 'TMR::translation')
+         {
+            var key_translation = localStorage.getItem('TMR::translation');
+            if (key_translation !== 'ru' ){
+               localStorage.removeItem('TMR::translation');
+               localStorage.setItem('TMR::translation', 'ru');
+            }
+         }
+       }
+  } else {
+    localStorage.setItem('TMR::translation', 'ru');
+  }
+}
+
+function translation_EN (){
+  var lang = 'en';
+
+  $('.lang').each(function(index, item) {
+    $(this).text(arrLang[lang][$(this).attr('key')]);
+  });
+  if (localStorage.length > 0)
+  {
+    for(let key in localStorage) {
+     if (!localStorage.hasOwnProperty(key)) {
+     continue; // пропустит такие ключи, как "setItem", "getItem" и так далее
+       }
+         if (key === 'TMR::translation')
+         {
+            var key_translation = localStorage.getItem('TMR::translation');
+            if (key_translation !== 'en' ){
+               localStorage.removeItem('TMR::translation');
+               localStorage.setItem('TMR::translation', 'en');
+            }
+         }
+       }
+  } else {
+    localStorage.setItem('TMR::translation', 'en');
+  }
+}
+
 var arrLang = {
   'en': {
     'login': 'Login',
     'register': 'Register',
     'knowledge_base': 'Knowledge base',
-    'contact': 'Contact US',
+    'search_projects': 'Search projects',
   },
   'ru': {
     'login': 'Вход',
     'register': 'Регистрация',
     'knowledge_base': 'База знаний',
-    'contact': 'Контакты',
+    'search_projects': 'Поиск',
   }
 };
 
-  $(function() {
-    $('.navbar-translation').click(function() {
-      var lang = $(this).attr('id');
+//  $(function() {
+//    $('.navbar-translation').click(function() {
+//      var lang = $(this).attr('id');
 
-      $('.lang').each(function(index, item) {
-        $(this).text(arrLang[lang][$(this).attr('key')]);
-      });
-    });
-  });
+//      $('.lang').each(function(index, item) {
+//        $(this).text(arrLang[lang][$(this).attr('key')]);
+//      });
+//    });
+//  });
