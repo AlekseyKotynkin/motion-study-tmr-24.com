05db1b9369becd0ae65755ff3a1a5e4658c3c893
AlekseyKotynkin <cay211076@gmail.com>
2022-10-19 08:43:26 +0300

    2022_10_19 08_43

--- webside/public/commonRegistrator.js
+++ webside/public/commonRegistrator.js
@@ -60,16 +60,16 @@ var translation_JS = localStorage.getItem('TMR::translation');
        }
       return;
      }
-     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-     if (!filter.test(email.value)) {
-         if(translation_JS == null || translation_JS == 'en'){
-           alert('Please provide a valid email address!');
-         } else {
-           alert('Пожалуйста, укажите действительный адрес электронной почты!');
-         }
-         email.focus;
-         return false;
-     }
+     // var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     // if (!filter.test(email.value)) {
+     //     if(translation_JS == null || translation_JS == 'en'){
+     //       alert('Please provide a valid email address!');
+     //     } else {
+     //       alert('Пожалуйста, укажите действительный адрес электронной почты!');
+     //     }
+     //     email.focus;
+     //     return false;
+     // }
     if (password.length < 4)
      {
        if(translation_JS == null || translation_JS == 'en'){
@@ -127,7 +127,7 @@ var translation_JS = localStorage.getItem('TMR::translation');
    function translationCommon_RU (){
      //
      var element = document.getElementById("registration_check");
-     var newElement = '<label class="form-check-label text-muted"><input type="checkbox" class="form-check-input" id="exampleInputTermsConditions" checked="checked"> Я согласен со всеми положениями и условиями </label>'
+     var newElement = '<label class="form-check-label text-muted"><input type="checkbox" class="form-check-input" id="exampleInputTermsConditions"> Я согласен со всеми положениями и условиями </label>'
      element.insertAdjacentHTML( 'afterbegin', newElement );
      //
      var element_1 = document.getElementById("registration_account_an");
@@ -138,7 +138,7 @@ var translation_JS = localStorage.getItem('TMR::translation');
    // заполняем строки с английскими значениями
    function translationCommon_EN (){
      var element = document.getElementById("registration_check");
-     var newElement = '<label class="form-check-label text-muted"><input type="checkbox" class="form-check-input" id="exampleInputTermsConditions" checked="checked"> I agree to all Terms & Conditions </label>'
+     var newElement = '<label class="form-check-label text-muted"><input type="checkbox" class="form-check-input" id="exampleInputTermsConditions"> I agree to all Terms & Conditions </label>'
      element.insertAdjacentHTML( 'afterbegin', newElement );
      //
      var element_1 = document.getElementById("registration_account_an");
