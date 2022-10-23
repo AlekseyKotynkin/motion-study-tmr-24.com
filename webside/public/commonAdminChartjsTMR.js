e5d6ce74169d4836bfa7c164202df751d11e104b
AlekseyKotynkin <cay211076@gmail.com>
2022-10-23 20:34:36 +0300

    2022_10_23 20_34

--- webside/public/commonAdminChartjsTMR.js
+++ webside/public/commonAdminChartjsTMR.js
@@ -605,6 +605,6 @@ function toComeInButtonUser(obj) {
   function translationCommon_EN (){
   }
   //
-  $(function(){
-      $("#datepicker").datepicker();
- });
+ //  $(function(){
+ //      $("#datepicker").datepicker();
+ // });
