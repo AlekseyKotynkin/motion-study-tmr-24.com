996b4819de3a65461532c2ab31fac4a685906289
AlekseyKotynkin <cay211076@gmail.com>
2022-10-24 11:57:40 +0300

    2022_10_24 11_57

--- webside/public/commonAdminChartjsTMR.js
+++ webside/public/commonAdminChartjsTMR.js
@@ -99,7 +99,7 @@ const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
     var objItem = obj.item;
     var idDocOrganization = obj.id;
     var nameOrganization = objItem.Organization;
-    itemsName.push({...{[idDocOrganization]: nameOrganization}});
+    itemsName.push({[idDocOrganization]: nameOrganization});
     //
     var tableMyOrganization = document.getElementById("tableAvalableSubdivision_Admin");
     for(var k = 1; k<tableMyOrganization.rows.length;){
@@ -123,7 +123,8 @@ const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
         console.log(doc.id, " => ", doc.data());
         var idDocSubdivision = doc.id;
         var nameSubdivision = doc.data().Subdivision;
-        itemsName.push({...{[idDocSubdivision]: nameSubdivision}});
+        // itemsName.push({...{[idDocSubdivision]: nameSubdivision}});
+        itemsName.push({[idDocSubdivision]: nameSubdivision});
         //получаем список должностей
         var docRefSubdivision = docRefOrganization.collection("Subdivision").doc(idDocSubdivision);
         docRefSubdivision.collection("Position").get().then((querySnapshot) => {
@@ -132,7 +133,8 @@ const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
             console.log(doc.id, " => ", doc.data());
             var idDocPosition = doc.id;
             var namePosition = doc.data().Position;
-            itemsName.push({...{[idDocPosition]: namePosition}});
+            // itemsName.push({...{[idDocPosition]: namePosition}});
+            itemsName.push({[idDocPosition]: namePosition});
             //получаем список пользователей
             var docRefPosition = docRefSubdivision.collection("Position").doc(idDocPosition);
             docRefPosition.collection("PositionUser").get().then((querySnapshot) => {
