a3e688b35dd55fde860d0d0eb83dcb6d1bfa3604
AlekseyKotynkin <cay211076@gmail.com>
2022-10-25 12:55:41 +0300

    2022_10_25 12_55

--- webside/public/commonAdminChartjsTMR.js
+++ webside/public/commonAdminChartjsTMR.js
@@ -377,110 +377,7 @@ const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
      for(var i = 1;i<tableDuble.rows.length;){
        tableDuble.deleteRow(i);
      }
-     ///заполняем круговую диаграмму
-     // Get context with jQuery - using jQuery's .get() method.
-     if ($("#barChart_Admin").length) {
-       var barChartCanvas = $("#barChart_Admin").get(0).getContext("2d");
-       // This will get the first returned node in the jQuery collection.
-       var barChart = new Chart(barChartCanvas, {
-         type: 'bar',
-         data: data,
-         options: options
-       });
-     }
-     var options = {
-       scales: {
-         yAxes: [{
-           ticks: {
-             beginAtZero: true
-           }
-         }]
-       },
-       legend: {
-         display: false
-       },
-       elements: {
-         point: {
-           radius: 0
-         }
-       }
 
-     };
-     var data = {
-       labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
-       datasets: [{
-         label: '# of Votes',
-         data: [10, 19, 3, 5, 2, 3],
-         backgroundColor: [
-           'rgba(255, 99, 132, 0.2)',
-           'rgba(54, 162, 235, 0.2)',
-           'rgba(255, 206, 86, 0.2)',
-           'rgba(75, 192, 192, 0.2)',
-           'rgba(153, 102, 255, 0.2)',
-           'rgba(255, 159, 64, 0.2)'
-         ],
-         borderColor: [
-           'rgba(255,99,132,1)',
-           'rgba(54, 162, 235, 1)',
-           'rgba(255, 206, 86, 1)',
-           'rgba(75, 192, 192, 1)',
-           'rgba(153, 102, 255, 1)',
-           'rgba(255, 159, 64, 1)'
-         ],
-         borderWidth: 1,
-         fill: false
-       }]
-     };
-
-
-     ///
-
-
-     ///заполняем диаграмму столбцами
-     if ($("#doughnutChart_Admin").length) {
-       var doughnutChartCanvas = $("#doughnutChart_Admin").get(0).getContext("2d");
-       var doughnutChart = new Chart(doughnutChartCanvas, {
-         type: 'doughnut',
-         data: doughnutPieData,
-         options: doughnutPieOptions
-       });
-     }
-     var doughnutPieData = {
-       datasets: [{
-         data: [30, 40, 30],
-         backgroundColor: [
-           'rgba(255, 99, 132, 0.5)',
-           'rgba(54, 162, 235, 0.5)',
-           'rgba(255, 206, 86, 0.5)',
-           'rgba(75, 192, 192, 0.5)',
-           'rgba(153, 102, 255, 0.5)',
-           'rgba(255, 159, 64, 0.5)'
-         ],
-         borderColor: [
-           'rgba(255,99,132,1)',
-           'rgba(54, 162, 235, 1)',
-           'rgba(255, 206, 86, 1)',
-           'rgba(75, 192, 192, 1)',
-           'rgba(153, 102, 255, 1)',
-           'rgba(255, 159, 64, 1)'
-         ],
-       }],
-
-       // These labels appear in the legend and in the tooltips when hovering different arcs
-       labels: [
-         'Pink',
-         'Blue',
-         'Yellow',
-       ]
-     };
-     var doughnutPieOptions = {
-       responsive: true,
-       animation: {
-         animateScale: true,
-         animateRotate: true
-       }
-     };
-     ///
      var docRefShift = db.collection("WorkShift").doc(nameDocShift);
      docRefShift.collection("ProcessUser").get().then(function(querySnapshot) {
        querySnapshot.forEach(function(doc) {
@@ -593,15 +490,15 @@ const FotoUrlLocalStorage = (LocalStorageValueObject[0]).photoUrl;
     var hours = Math.floor(timestamp / 60 / 60);
     if (hours < 10) {
       hours = '0' + hours
-    };
+    }
     var minutes = Math.floor(timestamp / 60) - (hours * 60);
     if (minutes < 10) {
       minutes = '0' + minutes
-    };
+    }
     var seconds = timestamp % 60;
     if (seconds < 10) {
       seconds = '0' + seconds
-    };
+    }
     var formatted = hours + ':' + minutes + ':' + seconds;
     var nameDocProcessButton = idEventItem.NameDocProcessButton;
     var processUserEnd = idEventItem.ProcessUserEnd;
@@ -749,3 +646,119 @@ function toComeInButtonUser(obj) {
   //   $('#datepicker').datepicker();
   //   $('#datepicker').datepicker('show');
   // });
+  $(function () {
+    /* ChartJS
+     * -------
+     * Data and config for chartjs
+     */
+    'use strict';
+    ///заполняем круговую диаграмму
+    // Get context with jQuery - using jQuery's .get() method.
+    if ($("#barChart_Admin").length) {
+      var barChartCanvas = $("#barChart_Admin").get(0).getContext("2d");
+      // This will get the first returned node in the jQuery collection.
+      var barChart = new Chart(barChartCanvas, {
+        type: 'bar',
+        data: data,
+        options: options
+      });
+    }
+
+    var options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      legend: {
+        display: false
+      },
+      elements: {
+        point: {
+          radius: 0
+        }
+      }
+
+    };
+    var data = {
+      labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
+      datasets: [{
+        label: '# of Votes',
+        data: [10, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1,
+        fill: false
+      }]
+    };
+
+
+    ///
+
+
+    ///заполняем диаграмму столбцами
+    if ($("#doughnutChart_Admin").length) {
+      var doughnutChartCanvas = $("#doughnutChart_Admin").get(0).getContext("2d");
+      var doughnutChart = new Chart(doughnutChartCanvas, {
+        type: 'doughnut',
+        data: doughnutPieData,
+        options: doughnutPieOptions
+      });
+    }
+
+    var doughnutPieData = {
+      datasets: [{
+        data: [30, 40, 30],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.5)',
+          'rgba(255, 206, 86, 0.5)',
+          'rgba(75, 192, 192, 0.5)',
+          'rgba(153, 102, 255, 0.5)',
+          'rgba(255, 159, 64, 0.5)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+      }],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+        'Pink',
+        'Blue',
+        'Yellow',
+      ]
+    };
+    var doughnutPieOptions = {
+      responsive: true,
+      animation: {
+        animateScale: true,
+        animateRotate: true
+      }
+    };
+    ///
+
+
+
+    });
